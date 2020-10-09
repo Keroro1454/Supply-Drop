12f7f6d08c1de539ad07244f0448000947d68750
Keroro1454 <71713549+Keroro1454@users.noreply.github.com>
2020-10-09 16:07:02 -0400

    Add files via upload

---
+++ Scripts/Bobbing.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Makes objects float up & down while gently spinning.
+public class Bobbing : MonoBehaviour
+{
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.3f;
+    public float frequency = 1f;
+
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    void Start()
+    {
+        posOffset = transform.position;
+    }
+    void Update()
+    {
+        if (Time.deltaTime == 0f | Time.fixedTime == 0f)
+        {
+            return;
+        }
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
---
+++ Scripts/Spin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class Spin : MonoBehaviour
+{
+
+	public float speed = 20;
+
+
+	void Update()
+	{
+		if (Time.deltaTime == 0f)
+		{
+			return;
+		}
+		transform.Rotate(Vector3.up, speed * Time.deltaTime);
+	}
+}
