03f88f9837079beea8e9664e34ae7acc539bf7d1
Keroro1454 <71713549+Keroro1454@users.noreply.github.com>
2020-10-20 16:55:38 -0400

    Add files via upload

---
+++ ItemHelpers.cs
@@ -0,0 +1,32 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace K1454.Utils
+{
+    public class ItemHelpers
+    {
+        public static CharacterModel.RendererInfo[] ItemDisplaySetup(GameObject obj)
+        {
+            MeshRenderer[] meshes = obj.GetComponentsInChildren<MeshRenderer>();
+            CharacterModel.RendererInfo[] renderInfos = new CharacterModel.RendererInfo[meshes.Length];
+
+            for (int i = 0; i < meshes.Length; i++)
+            {
+                renderInfos[i] = new CharacterModel.RendererInfo
+                {
+                    defaultMaterial = meshes[i].material,
+                    renderer = meshes[i],
+                    defaultShadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
+                    ignoreOverlays = false
+                };
+            }
+
+            return renderInfos;
+
+        }
+
+    }
+}
