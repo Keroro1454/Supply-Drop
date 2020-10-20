c49f9b3ec9eb0cf1c596a0278507e3907322d188
Keroro1454 <71713549+Keroro1454@users.noreply.github.com>
2020-10-20 15:04:16 -0400

    Add files via upload

--- Items/UnassumingTie.cs
+++ Items/UnassumingTie.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using TILER2;
 using static TILER2.StatHooks;
 using K1454.SupplyDrop;
+using K1454.Utils;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using System;
@@ -13,24 +14,24 @@ using System.Reflection;
 
 namespace SupplyDrop.Items
 {
-    class UnassumingTie : Item<UnassumingTie>
+    class UnassumingTie : Item_V2<UnassumingTie>
     {
         public override string displayName => "Unassuming Tie";
 
-        public override ItemTier itemTier => RoR2.ItemTier.Tier1;
+        public override ItemTier itemTier => ItemTier.Tier1;
 
         public override ReadOnlyCollection<ItemTag> itemTags => new ReadOnlyCollection<ItemTag>(new[] { ItemTag.Utility });
-        protected override string NewLangName(string langid = null) => displayName;
+        protected override string GetNameString(string langid = null) => displayName;
 
-        protected override string NewLangPickup(string langID = null) => "Gain some shield, and receive a speed boost when your shield is broken.";
+        protected override string GetPickupString(string langID = null) => "Gain some shield, and receive a speed boost when your shield is broken.";
 
-        protected override string NewLangDesc(string langID = null) => "Gain a <style=cIsUtility>shield</style> equal to <style=cIsUtility>4%</style> <style=cStack>(+4% per stack)</style> of your maximum health. " +
+        protected override string GetDescString(string langID = null) => "Gain a <style=cIsUtility>shield</style> equal to <style=cIsUtility>4%</style> <style=cStack>(+2% per stack)</style> of your maximum health. " +
             "Breaking your <style=cIsUtility>shield</style> gives you a <style=cIsUtility>Second Wind</style> for 4s, plus a bonus amount based on your <style=cIsUtility>maximum shield</style>. " +
             "Second Wind increases <style=cIsUtility>movement speed</style> by <style=cIsUtility>15%</style> <style=cStack>(+10% per stack)</style>.";
 
-        protected override string NewLangLore(string landID = null) => "\"This necktie was a staple accessory of one of a notorious group of well-dressed heisters which were active during the early 21st century.The gang was wildly successful while active, breaking into, looting, and escaping from some of the most secure sites on Earth at the time. Even when authorities attempted to apprehend the criminals, reports state that shooting at them 'only seem to make [the heisters] move faster, however the hell that works.' While the identities of these criminals were never discovered, the gang ceased operations for unknown reasons after over a decade of activity. This piece serves as a testament to their dedication to style, no matter the situation.\"\n\n- Placard description for \"Striped Tie\" at the Galactic Museum of Law Enforcement and Criminality";
+        protected override string GetLoreString(string landID = null) => "\"This necktie was a staple accessory of one of a notorious group of well-dressed heisters which were active during the early 21st century.The gang was wildly successful while active, breaking into, looting, and escaping from some of the most secure sites on Earth at the time. Even when authorities attempted to apprehend the criminals, reports state that shooting at them 'only seem to make [the heisters] move faster, however the hell that works.' While the identities of these criminals were never discovered, the gang ceased operations for unknown reasons after over a decade of activity. This piece serves as a testament to their dedication to style, no matter the situation.\"\n\n- Placard description for \"Striped Tie\" at the Galactic Museum of Law Enforcement and Criminality";
 
-        private static List<RoR2.CharacterBody> Playername = new List<RoR2.CharacterBody>();
+        private static List<CharacterBody> Playername = new List<CharacterBody>();
         public static GameObject ItemBodyModelPrefab;
         public static GameObject SecondItemBodyModelPrefab;
         public static GameObject ThirdItemBodyModelPrefab;
@@ -40,40 +41,45 @@ namespace SupplyDrop.Items
 
         public UnassumingTie()
         {
-            modelPathName = "@SupplyDrop:Assets/Main/Models/Prefabs/TiePickUp.prefab";
-            
-            iconPathName = "@SupplyDrop:Assets/Main/Textures/Icons/TieIcon.png";
-
-            onAttrib += (tokenIdent, namePrefix) =>
+            modelResourcePath = "@SupplyDrop:Assets/Main/Models/Prefabs/TiePickUp.prefab";
+            iconResourcePath = "@SupplyDrop:Assets/Main/Textures/Icons/TieIcon.png";
+        }
+        public override void SetupAttributes()
+        {
+            if (ItemBodyModelPrefab == null)
             {
-                var secondWindBuff = new R2API.CustomBuff(
+                ItemBodyModelPrefab = Resources.Load<GameObject>(modelResourcePath);
+                displayRules = GenerateItemDisplayRules();
+            }
+
+            base.SetupAttributes();
+            var secondWindBuff = new R2API.CustomBuff(
                     new BuffDef
                     {
                         canStack = false,
                         isDebuff = false,
-                        name = namePrefix + "SecondWindBuff",
+                        name = "SecondWindBuff",
                         iconPath = "@SupplyDrop:Assets/Main/Textures/Icons/SecondWindBuffIcon.png"
                     });
-                SecondWindBuff = R2API.BuffAPI.Add(secondWindBuff);
+                SecondWindBuff = BuffAPI.Add(secondWindBuff);
 
                 var windedDebuff = new R2API.CustomBuff(
                     new BuffDef
                     {
                         canStack = false,
                         isDebuff = true,
-                        name = namePrefix + "WindedDebuff",
+                        name = "WindedDebuff",
                         iconPath = "@SupplyDrop:Assets/Main/Textures/Icons/WindedDebuffIcon.png"
                     });
-                WindedDebuff = R2API.BuffAPI.Add(windedDebuff);
-            };
+                WindedDebuff = BuffAPI.Add(windedDebuff);
         }
 
         private static ItemDisplayRuleDict GenerateItemDisplayRules()
         {
             ItemBodyModelPrefab.AddComponent<ItemDisplay>();
-            ItemBodyModelPrefab.GetComponent<ItemDisplay>().rendererInfos = SupplyDropPlugin.ItemDisplaySetup(ItemBodyModelPrefab);
             SecondItemBodyModelPrefab.AddComponent<ItemDisplay>();
-            SecondItemBodyModelPrefab.GetComponent<ItemDisplay>().rendererInfos = SupplyDropPlugin.ItemDisplaySetup(SecondItemBodyModelPrefab);
+            ItemBodyModelPrefab.GetComponent<ItemDisplay>().rendererInfos = ItemHelpers.ItemDisplaySetup(ItemBodyModelPrefab);
+            SecondItemBodyModelPrefab.GetComponent<ItemDisplay>().rendererInfos = ItemHelpers.ItemDisplaySetup(SecondItemBodyModelPrefab);
 
             Vector3 generalScale = new Vector3(1f, 1f, 1f);
             ItemDisplayRuleDict rules = new ItemDisplayRuleDict(new ItemDisplayRule[]
@@ -198,30 +204,32 @@ namespace SupplyDrop.Items
             });
             return rules;
         }
-        protected override void LoadBehavior()
+        public override void Install()
         {
+            base.Install();
             ItemBodyModelPrefab = Resources.Load<GameObject>("@SupplyDrop:Assets/Main/Models/Prefabs/Tie.prefab");
             SecondItemBodyModelPrefab = Resources.Load<GameObject>("@SupplyDrop:Assets/Main/Models/Prefabs/TieBig.prefab");
             if (ThirdItemBodyModelPrefab == null)
             {
-                ThirdItemBodyModelPrefab = regDef.pickupModelPrefab;
-                regItem.ItemDisplayRules = GenerateItemDisplayRules();
+                ThirdItemBodyModelPrefab = itemDef.pickupModelPrefab;
+                customItem.ItemDisplayRules = GenerateItemDisplayRules();
             }
-            regDef.pickupModelPrefab.transform.localScale = new Vector3(3f, 3f, 3f);
+            itemDef.pickupModelPrefab.transform.localScale = new Vector3(3f, 3f, 3f);
             On.RoR2.HealthComponent.TakeDamage += CalculateBuff;
             IL.RoR2.CharacterBody.RecalculateStats += IL_AddMaxShield;
             GetStatCoefficients += AddSecondWindBuff;
             On.RoR2.CharacterBody.RemoveBuff += AddWindedDebuff;
         }
 
-        protected override void UnloadBehavior()
+        public override void Uninstall()
         {
+            base.Uninstall();
             IL.RoR2.CharacterBody.RecalculateStats -= IL_AddMaxShield;
             On.RoR2.HealthComponent.TakeDamage -= CalculateBuff;
             GetStatCoefficients -= AddSecondWindBuff;
             On.RoR2.CharacterBody.RemoveBuff -= AddWindedDebuff;
         }
-        private void AddWindedDebuff(On.RoR2.CharacterBody.orig_RemoveBuff orig, RoR2.CharacterBody self, BuffIndex buffType)
+        private void AddWindedDebuff(On.RoR2.CharacterBody.orig_RemoveBuff orig, CharacterBody self, BuffIndex buffType)
         {
             orig(self, buffType);
             if (buffType == SecondWindBuff)
@@ -244,14 +252,14 @@ namespace SupplyDrop.Items
             {
                 if (GetCount(characterBody) > 0)
                 {
-                    return shield + (characterBody.maxHealth * (0.04f * GetCount(characterBody)));
+                    return shield + (characterBody.maxHealth * (0.04f + (0.02f * (GetCount(characterBody) - 1))));
                 }
                 return shield;
             }
             );
             c.Emit(OpCodes.Stloc, 43);
         }
-        private void CalculateBuff(On.RoR2.HealthComponent.orig_TakeDamage orig, RoR2.HealthComponent self, RoR2.DamageInfo damageInfo)
+        private void CalculateBuff(On.RoR2.HealthComponent.orig_TakeDamage orig, HealthComponent self, DamageInfo damageInfo)
         {
             var preDamageShield = self.body.healthComponent.shield; 
             orig(self, damageInfo);
