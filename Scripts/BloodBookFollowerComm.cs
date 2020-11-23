d92108057ff4ba5e736417bffa38029a36646eb7
Keroro1454 <mattp1454@gmail.com>
2020-11-23 17:24:16 -0500

    Updated/Renamed Util/Main Files

---
+++ Scripts/BloodBookFollowerComm.cs
@@ -0,0 +1,23 @@
+﻿using RoR2;
+using UnityEngine;
+
+namespace SupplyDrop.Utils
+{
+    
+    public class BloodBookFollowerComm : MonoBehaviour
+    {
+        void Start()
+        {
+
+            var itemFollower = GetComponent<ItemFollower>();
+            if (itemFollower)
+            {
+                var followerBleed = itemFollower.followerPrefab.GetComponent<BleedingScript>();
+                if (followerBleed)
+                {
+                    followerBleed.model = GetComponentInParent<CharacterModel>();
+                }
+            }
+        }
+    }
+}
