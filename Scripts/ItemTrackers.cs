9d045d4b37f49cb8b7a5d04795b8527d8f5c9d49
Keroro1454 <mattp1454@gmail.com>
2021-01-01 19:25:33 -0500

    Create ItemTrackers.cs

---
+++ Scripts/ItemTrackers.cs
@@ -0,0 +1,13 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace SupplyDrop.Utils
+{
+    public class ItemTrackers : MonoBehaviour
+    {
+        public int damageItemCount { get; set; }
+    }
+}
