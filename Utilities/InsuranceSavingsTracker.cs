5641eff1db1c92b10fff6247e0ce45f0d3dc59ae
Keroro1454 <mattp1454@gmail.com>
2021-01-13 02:57:36 -0500

    Renamed Scripts to Utilities
    Change makes things more in-line with the naming conventions used in the code

---
+++ Utilities/InsuranceSavingsTracker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace SupplyDrop.Utils
+{
+    public class InsuranceSavingsTracker : MonoBehaviour
+    {
+        public uint insuranceSavings { get; set; }
+    }
+}
