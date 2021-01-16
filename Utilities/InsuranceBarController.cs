644ea463d8b6051e53976ccc9240a9c18fff1108
Keroro1454 <mattp1454@gmail.com>
2021-01-16 18:22:05 -0500

    Create InsuranceBarController.cs

---
+++ Utilities/InsuranceBarController.cs
@@ -0,0 +1,25 @@
+﻿using RoR2;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using SupplyDrop.Items;
+
+namespace SupplyDrop.Utilities
+{
+    public class InsuranceBarController : MonoBehaviour
+    {
+        public CharacterBody body;
+
+        public void FixedUpdate()
+        {
+            var characterBody = body;
+            if (body)
+            {
+                var healthBar = GetComponent<Slider>();
+                healthBar.maxValue = body.maxHealth;
+                healthBar.value = body.healthComponent.health;
+            }
+        }
+    }
+}
