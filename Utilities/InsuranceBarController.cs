0c3dfcdafbd43d06b4c4846a893b65564e06729b
Keroro1454 <mattp1454@gmail.com>
2021-01-16 22:33:03 -0500

    Update InsuranceBarController.cs

--- Utilities/InsuranceBarController.cs
+++ Utilities/InsuranceBarController.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 using System;
 using System.Linq;
 using SupplyDrop.Items;
+using SupplyDrop.Utils;
 
 namespace SupplyDrop.Utilities
 {
@@ -16,9 +17,24 @@ namespace SupplyDrop.Utilities
             var characterBody = body;
             if (body)
             {
-                var healthBar = GetComponent<Slider>();
-                healthBar.maxValue = body.maxHealth;
-                healthBar.value = body.healthComponent.health;
+                var insuranceBar = GetComponent<Slider>();
+
+                //Acquire components for calculations
+                var cachedSavingsComponent = body.gameObject.GetComponent<InsuranceSavingsTracker>();
+                if (!cachedSavingsComponent)
+                {
+                    cachedSavingsComponent = body.gameObject.AddComponent<InsuranceSavingsTracker>();
+                }
+                var itemComponent = body.gameObject.GetComponent<HolyInsurance>();
+
+                //Finds index where current savings amounts fits in
+                int topTierAffordable = Array.FindIndex(itemComponent.ranges, r => r.Contains(cachedSavingsComponent.insuranceSavings));
+                //Takes the index we just found and determines what the Upper value in that index is
+                var insuranceBarMax = itemComponent.ranges.FirstOrDefault(r => r.Upper == topTierAffordable).Upper;
+                //Convert that Upper value from a double to a float/single
+                insuranceBar.maxValue = Convert.ToSingle(insuranceBarMax);
+                //Tells the bar what value we actually have
+                insuranceBar.value = cachedSavingsComponent.insuranceSavings;
             }
         }
     }
